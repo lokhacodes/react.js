5824e38547f0285bb012c15ce1bfb580385a258e
lokhacodes <sumaiyasonali421@gmail.com>
2026-01-29 22:03:54 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css'
+
+
+const root = createRoot(document.getElementById("root"));
+root.render(<h1>Hello, Static Pages in React!</h1>);
