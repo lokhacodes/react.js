219369ce94b2922d11025514bc187eb065277d03
lokhacodes <sumaiyasonali421@gmail.com>
2026-02-17 05:34:02 +0600

    fragments, custom components-Parent/child component

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'
+//import { Fragment } from 'react';
 
 
 const root = createRoot(document.getElementById("root"));
@@ -8,23 +9,41 @@ root.render(
    <Page />
 )
 
-
-export default function Page (){
+function Header(){
     return (
-       <>
-            <header>
+         <header>
                 <img src="src/assets/react-logo.png" width="40px" alt="React logo" />
             </header>
-            <main>
+        )}
+
+
+function MainContent(){
+    return(
+   <main>
                 <h1>Reason I am excited to learn React</h1>
                 <ol>
                     <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
                     <li>I am more likely to get a job as a front end developer if I know React</li>
                 </ol>
             </main>
-            <footer>
+    )
+}
+
+function Footer(){
+    return(
+         <footer>
                 <small>© 2026 Lokha development. All rights reserved.</small>
             </footer>
+    )
+}
+
+export default function Page (){
+    return (
+       <>
+            <Header />
+            <MainContent />
+            <Footer />
+           
         </>
     )
 }
