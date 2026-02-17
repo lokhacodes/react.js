f0c22eeb804593a0f8a38e4aad9fed31e68abc64
lokhacodes <sumaiyasonali421@gmail.com>
2026-02-17 06:01:26 +0600

    Styling with classes

---
+++ src/Footer
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+         <footer>
+                <small>© 2026 Lokha development. All rights reserved.</small>
+            </footer>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,40 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css'
 //import { Fragment } from 'react';
-
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 const root = createRoot(document.getElementById("root"));
 root.render(
    <Page />
 )
 
-function Header(){
-    return (
-         <header>
-                <img src="src/assets/react-logo.png" width="40px" alt="React logo" />
-            </header>
-        )}
 
 
-function MainContent(){
-    return(
-   <main>
-                <h1>Reason I am excited to learn React</h1>
-                <ol>
-                    <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
-                    <li>I am more likely to get a job as a front end developer if I know React</li>
-                </ol>
-            </main>
-    )
-}
-
-function Footer(){
-    return(
-         <footer>
-                <small>© 2026 Lokha development. All rights reserved.</small>
-            </footer>
-    )
-}
 
 export default function Page (){
     return (
