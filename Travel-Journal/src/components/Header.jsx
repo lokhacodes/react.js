94deef97beca121a6b8f46609141e3f8bda4cf5e
lokhacodes <sumaiyasonali421@gmail.com>
2026-02-18 06:22:24 +0600

    header

---
+++ Travel-Journal/src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <>
+          <header className="header">
+            <img src="src/assets/globe.png" alt="globe" width ="40px" />
+            <h1>My Travel Journal</h1>
+          </header>
+         
+        </>
+    )
+}
