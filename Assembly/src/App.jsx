6c70d609fcf0faac6b7a98d56ae1218d0bd9b3b6
lokhacodes <sumaiyasonali421@gmail.com>
2026-05-15 21:56:02 +0600

    frontend

--- Assembly/src/App.jsx
+++ Assembly/src/App.jsx
@@ -1,13 +1,57 @@
-import React from "react"
-export default function AssemblyEndgame(){
+import { useState } from "react"
+import { languages } from "./languages"
 
-   return (
+export default function AssemblyEndgame() {
+  const [currentWord, setCurrentWord] = useState("react")
+  
+  const alphabet = "abcdefghijklmnopqrstuvwxyz"
+  const keyboardElements = alphabet.split("").map(letter => (
+    <button key={letter}>{letter.toUpperCase()}</button>
+  ))
+
+  const languageElements = languages.map((lang) => {
+    const styles = {
+      backgroundColor: lang.backgroundColor,
+      color: lang.color,
+    }
+
+    return (
+      <span className="chip" style={styles} key={lang.name}>
+        {lang.name}
+      </span>
+    )
+  })
+
+  const letterElements = currentWord.split("").map ((letter, index) => (
+    <span key={index}>{letter.toUpperCase()}</span>
+  ))
+
+  return (
     <main>
-       <header>
-         <h1> Game goes here</h1>
-         <p> Guess the word within 8 attempts to keep the programming world safe from assembly</p>
-       </header>
-     
+      <header>
+        <h1> Game goes here</h1>
+        <p>
+          Guess the word within 8 attempts to keep the programming world safe from
+          assembly
+        </p>
+      </header>
+
+      <section className="game-status">
+        <h2>You win!! 🎉</h2>
+        <p>Well done!</p>
+      </section>
+
+       
+
+      <section className="language-chips">{languageElements}</section>
+      <section className="word">
+        {letterElements}
+      </section>
+      <section className="keyboard">
+          {keyboardElements}
+      </section>
+      <button className="new-game">New Game</button>
     </main>
-   )
+  )
 }
+
